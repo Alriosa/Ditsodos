554a3988e122e6dd7535f235360f2c45e1562baa
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-10-04 09:44:24 -0600

    1

--- Ditsodos/Login.Designer.cs
+++ Ditsodos/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ditsodos
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.Location = new System.Drawing.Point(452, 160);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(75, 81);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "metroTile1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroTile1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
     }
 }
 
--- Ditsodos/Login.cs
+++ Ditsodos/Login.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Ditsodos
 {
-    public partial class Form1 : Form
+    public partial class Login : MetroFramework.Forms.MetroForm
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
